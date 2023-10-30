0d4f657118fd3b9f606b91a12a2dd3cd58b0958d
Vadim Kaluga <kalugavadim04@gmail.com>
2023-10-30 11:52:42 +0700

    four

--- petrov.py
+++ petrov.py
@@ -1,4 +1,5 @@
 from Job import Job, jobs_copy
+import math
 
 def petrov(jobs: list[Job]) -> list[Job]:
     '''Получение лучшего расписания по правилам Петрова'''
@@ -6,6 +7,7 @@ def petrov(jobs: list[Job]) -> list[Job]:
     D_012 = first_petrov(D_01=D_01, D_2=D_2)
     D = second_petrov(D_012=D_012)
     third = third_petrov(D_1=D_1, D_0=D_0, D_2=D_2)
+    fourth = fourth_petrov(D_1=D_1, D_2=D_2, D_0=D_0, D_01=D_01)
 
 def D_sets(jobs: list[Job]) -> tuple[list[Job], list[Job],
                                      list[Job], list[Job]]:
@@ -27,8 +29,7 @@ def D_sets(jobs: list[Job]) -> tuple[list[Job], list[Job],
 
 def first_petrov(D_01: list[Job], D_2: list[Job]) -> list[Job]:
     '''Ранжирование D_01 по возарастанию p_1 и D_2 по убыванию p_2 c последующим
-    объединением D_012 = D_01 + D_2'''
-    # сортировка D_01 по возрастанию p_1
+    объединением D_012 = D_01 + D_2''' # сортировка D_01 по возрастанию p_1
     sorted_D_01 = sorted(D_01, key=lambda job: job.p_1)
     # сортировка D_2 по убыванию p_2
     sorted_D_2 = sorted(D_2, key=lambda job: job.p_2, reverse=True)
@@ -47,5 +48,75 @@ def third_petrov(D_1: list[Job], D_0: list[Job], D_2: list[Job]) -> list[Job]:
     sorted_D_2 = sorted(D_2, key=lambda job: job.p_2, reverse=True)
     return [*sorted_D_1, *sorted_D_0, *sorted_D_2]
 
-def fourth_petrov(D_1: list[Job], D_2: list[Job], D_0: list[Job], D_01: list[Job]) -> list[Job]:
-    pass
+def fourth_petrov(D_1: list[Job], D_2: list[Job], 
+                  D_0: list[Job], D_01: list[Job]) -> list[Job]:
+    '''Производится упорядочивание по парам'''
+    D_1_sorted_p1 = sorted(D_1, key=lambda job: job.p_1) 
+    D_1_sorted_p2 = sorted(D_1, key=lambda job: job.p_2, reverse=True)
+
+    D_0_sorted_p1 = sorted(D_0, key=lambda job: job.p_1) 
+    D_0_sorted_p2 = sorted(D_0, key=lambda job: job.p_2, reverse=True)
+
+    D_2_sorted_p1 = sorted(D_2, key=lambda job: job.p_1)
+    D_2_sorted_p2 = sorted(D_2, key=lambda job: job.p_2, reverse=True)
+
+    # формирование пар из D_1
+    couples_D_1 = []
+    for i in range(len(D_1)):
+        if D_1_sorted_p1[i].index != D_1_sorted_p2[i].index:
+            couples_D_1.append([D_1_sorted_p1[i], D_1_sorted_p2[i]]) 
+        else:
+            couples_D_1.append([D_1_sorted_p1[i], None])
+    couples_D_1 = couples_D_1[:math.ceil(len(D_1)/2)]
+
+    # если количество деталей в D_1 нечётно (одна пара пустая)
+    # то в полупустую пару добавляется элемент из D_0 или D_2
+    if len(D_1) % 2 == 1:
+        if len(D_0) != 0:
+            couples_D_1[-1][1] = D_0_sorted_p1[0]
+            D_0_sorted_p1.pop(0)
+        elif len(D_2) != 0:
+            couples_D_1[-1][1] = D_2_sorted_p1[0]
+            D_2_sorted_p1.pop(0)
+
+    # формирование пар из остатков D_0
+    couples_D_0 = []
+    for i in range(len(D_0_sorted_p1)):
+        if D_0_sorted_p1[i].index != D_0_sorted_p2[i].index:
+            couples_D_0.append([D_0_sorted_p1[i], D_0_sorted_p2[i]])
+        else:
+            couples_D_0.append([D_0_sorted_p1[i], None])
+
+    # если количество деталей в D_0 нечётно (одна пара пустая)
+    # то в полупустую пару добавляется элемент из D_2
+    if len(D_0_sorted_p1) % 2 == 1:
+        if len(D_2_sorted_p1) != 0:
+            couples_D_0[-1][1] = D_2_sorted_p1[0]
+            D_2_sorted_p1.pop(0)
+
+    # формирование пар из остатков D_2
+    couples_D_2 = []
+    for i in range(len(D_2_sorted_p1)):
+        if D_2_sorted_p1[i].index != D_2_sorted_p2[i].index:
+            couples_D_2.append([D_2_sorted_p1[i], D_2_sorted_p2[i]])
+        else:
+            couples_D_2.append([D_2_sorted_p1[i], None])
+
+    couples = [*couples_D_1, *couples_D_0, *couples_D_2]
+    print(couples)
+    # TODO: написать вклинивание
+    # вклинивание остатка если таковой имеется
+    #if couples[-1][1] == None:
+    #    i_clin = 0
+    #    max_lmbd_i = 0
+    #    min_lmbd_i = couples[0][0].p_2 - couples[0][0].p_1
+    #    lmbd = couples[-1][0].p_2 - couples[-1][0].p_1 
+    #    for i in range(len(couples)-1):
+    #       lmbd_i = couples[i][0].p_2 - couples[i][0].p_1 
+    #       lmbd_i_next = couples[i+1].p_2 - couples[i+1].p_1 
+    #       if max_lmbd_i <= lmbd_i and min_lmbd_i >= lmbd_i_next \
+    #               and lmbd < max_lmbd_i and lmbd > min_lmbd_i:
+    #           i_clin = i
+    #           max_lmbd_i = lmbd_i
+    #           min_lmbd_i = lmbd_i_next
+               
